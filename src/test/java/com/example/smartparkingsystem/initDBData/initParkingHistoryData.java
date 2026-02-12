cc89a15cb536358f6c3051f131ac00964b0cd1d9
mmmjjjson <rmflqjf@naver.com>
2026-02-12 11:56:59 +0900

    0212_테스트데이터

--- src/test/java/com/example/smartparkingsystem/initDBData/initParkingHistoryData.java
+++ src/test/java/com/example/smartparkingsystem/initDBData/initParkingHistoryData.java
@@ -42,6 +42,22 @@ class initParkingHistoryData {
                 }
             }
         }
-    }
 
+        int monthlyCount = 20;
+        for (int i = 0; i < monthlyCount; i++) {
+            // 주차 시간 랜덤 설정
+            LocalDateTime entryTime = LocalDateTime.of(2026, 1,
+                    random.nextInt(28) + 1, random.nextInt(24), random.nextInt(60));
+            int totalMinutes = random.nextInt(2850) + 30;
+            LocalDateTime exitTime = entryTime.plusMinutes(totalMinutes);
+
+            ParkingHistoryVO parkingHistoryVO = ParkingHistoryVO.builder()
+                    .parkingArea("A-" + (random.nextInt(20) + 1))
+                    .carNum((random.nextInt(90) + 10) + "가" + (1000 + random.nextInt(9000)))
+                    .carType(new String[]{"일반", "경차", "장애인"}[random.nextInt(3)])
+                    .entryTime(entryTime).exitTime(exitTime).totalMinutes(totalMinutes).build();
+
+            parkingHistoryDAO.insertTestData(parkingHistoryVO);
+        }
+    }
 }
