74396ca693e7aa1f0e7dff1896f14047586f4589
vcg258 <vcg258@naver.com>
2026-02-19 11:10:37 +0900

    26-02-19

--- src/main/java/com/example/smartparkingsystem/DB/validation.sql
+++ src/main/java/com/example/smartparkingsystem/DB/validation.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `validation`
 (
-    `no`           INT PRIMARY KEY AUTO_INCREMENT COMMENT '인덱스',
+    `no`           BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '인덱스',
     `admin_id`     VARCHAR(20) NOT NULL COMMENT 'FK 관리자 아이디',
     `otp_code`     CHAR(6)     NOT NULL COMMENT 'OTP',
     `admin_email`  VARCHAR(50) NOT NULL COMMENT '관리자 이메일 (OTP보낸 이메일)',
