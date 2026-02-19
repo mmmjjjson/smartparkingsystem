7db0627787bb27155778410c3629441e81d6ae4d
Jinhyelim <sjk04069@naver.com>
2026-02-19 15:01:28 +0900

    Update ConnectionUtil.java

--- src/main/java/com/example/smartparkingsystem/util/ConnectionUtil.java
+++ src/main/java/com/example/smartparkingsystem/util/ConnectionUtil.java
@@ -15,7 +15,7 @@ public enum ConnectionUtil {
         HikariConfig config = new HikariConfig();
         config.setDriverClassName("org.mariadb.jdbc.Driver");
         config.setJdbcUrl("jdbc:mariadb://localhost:3306/smart_parking_system");
-        config.setUsername("sps_user");
+        config.setUsername("system_user");
         config.setPassword("0220");
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
