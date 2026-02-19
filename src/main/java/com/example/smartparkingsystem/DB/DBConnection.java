7b6e0475d687288f3a4936a5424f8c34ef335f4f
mmmjjjson <rmflqjf@naver.com>
2026-02-19 14:49:24 +0900

    0219

--- src/main/java/com/example/smartparkingsystem/DB/DBConnection.java
+++ src/main/java/com/example/smartparkingsystem/DB/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Connection connection = null;
         String url = "jdbc:mariadb://localhost:3306/smart_parking_system";
-        String user = "systemUser";
+        String user = "system_user";
         String pass = "0220";
 
         Class.forName("org.mariadb.jdbc.Driver");
